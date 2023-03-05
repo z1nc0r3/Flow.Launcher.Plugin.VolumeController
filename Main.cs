99af16e35ecd1aa64274b5f25bb6d19bed2a4e81
Lasith Manujitha <Manujitha98@gmail.com>
2023-03-05 19:01:13 +0530

    Code refactor

--- Main.cs
+++ Main.cs
@@ -1,159 +1,147 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Windows;
-using Flow.Launcher.Plugin;
-using System.Diagnostics;
 using NAudio.CoreAudioApi;
-using System.Net;
-using System.ComponentModel;
-using System.Collections;
-using System.Linq;
-using FuzzySharp;
-using System.Text.RegularExpressions;
-using NAudio.Utils;
 
-namespace Flow.Launcher.Plugin.VolumeController {
-    public class VolumeController : IPlugin {
+namespace Flow.Launcher.Plugin.VolumeController;
 
-        private PluginInitContext context;
+public class VolumeController : IPlugin
+{
+    private MMDevice _defaultPlaybackDevice;
+    private readonly MMDeviceEnumerator _deviceEnumerator = new();
+    private AudioEndpointVolume _volume;
 
-        private readonly List<Result> _results = new List<Result>();
-        private MMDeviceEnumerator _deviceEnumerator = new MMDeviceEnumerator();
-        private MMDevice _defaultPlaybackDevice;
-        private AudioEndpointVolume _volume;
+    private PluginInitContext _context;
 
-        private float currentVolume;
-        private bool isMute;
+    private float _currentVolume;
+    private bool _isMute;
 
-        public void Init(PluginInitContext context) {
-            this.context = context;
-            _defaultPlaybackDevice = _deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            _volume = _defaultPlaybackDevice.AudioEndpointVolume;
+    public void Init(PluginInitContext context)
+    {
+        this._context = context;
+        _defaultPlaybackDevice = _deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+        _volume = _defaultPlaybackDevice.AudioEndpointVolume;
+    }
 
-            if (_defaultPlaybackDevice == null) {
-                throw new Exception("No audio endpoint found.");
-            }
-            _volume = _defaultPlaybackDevice.AudioEndpointVolume;
-            if (_volume == null) {
-                throw new Exception("Failed to get audio volume control.");
-            }
-        }
+    public List<Result> Query(Query query)
+    {
+        var commands = Commands(query);
 
-        public List<Result> Query(Query query) {
-            var commands = Commands(query);
-            return commands;
-        }
-
-        private List<Result> Commands(Query query) {
-            var results = new List<Result>();
-
-            // Get the search query from user input
-            string search = query.Search.Trim().ToLower();
-
-            // Check if user typed "mute" or a similar keyword
-            if (search.StartsWith("m")) {
-                results.Add(new Result {
-                    Title = "Mute",
-                    SubTitle = "Mute the volume",
-                    Action = c => {
-                        Mute();
-                        return true;
-                    }
-                });
-            } else if (search.StartsWith("u")) {
-                results.Add(new Result {
-                    Title = "Unmute",
-                    SubTitle = "Restore the volume",
-                    Action = c => {
-                        Unmute();
-                        return true;
-                    }
-                });
-            } else if (double.TryParse(query.Search, out double value)) {
-                results.Add(new Result {
-                    Title = "Set Volume",
-                    SubTitle = $"Set volume to {value}",
-                    Action = c => {
-                        SetVolume(value);
-                        return true;
-                    },
-                    IcoPath = "Images/app.png"
-                });
-            } else {
-                results.AddRange(new[]
+        if (_defaultPlaybackDevice == null || _volume == null)
+            return new List<Result>
+            {
+                new()
                 {
-                    new Result
-                    {
-                        Title = "Mute",
-                        SubTitle = "Mute the volume",
-                        Action = c =>
-                        {
-                            Mute();
-                            return true;
-                        }
-                    },
-                    new Result
-                    {
-                        Title = "Unmute",
-                        SubTitle = "Restore the volume",
-                        Action = c =>
-                        {
-                            Unmute();
-                            return true;
-                        }
-                    },
-                    new Result
-                    {
-                        Title = "Set Volume",
-                        SubTitle = $"Query: {(query.Search)}",
-                        Action = c =>
-                        {
-                            SetVolume(double.Parse(query.Search));
-                            return true;
-                        }
-                    }
-                });
-
-                if (query.Search.Length > 0 && !double.TryParse(query.Search, out double val)) {
-                    foreach (var result in results) {
-                        if (result.Title.Equals("Set Volume")) {
-                            results.Remove(result);
-                            break;
-                        }
-                    }
+                    Title = "No audio endpoint found.",
+                    IcoPath = _context.CurrentPluginMetadata.IcoPath
                 }
+            };
+
+        return commands;
+    }
+
+    private List<Result> Commands(Query query)
+    {
+        var results = new List<Result>();
+
+        var search = query.Search.Trim().ToLower();
+
+        if (search.StartsWith("m"))
+        {
+            AddMuteResult(results);
+        }
+        else if (search.StartsWith("u"))
+        {
+            AddUnmuteResult(results);
+        }
+        else if (double.TryParse(query.Search, out var value))
+        {
+            AddSetVolumeResult(results, value);
+        }
+        else
+        {
+            AddMuteResult(results);
+            AddUnmuteResult(results);
+            try
+            {
+                AddSetVolumeResult(results, double.Parse(query.Search));
             }
-            
-            return results;
+            catch
+            {
+                AddSetVolumeResult(results, -1);
+            }
+
+            if (query.Search.Length > 0 && !double.TryParse(query.Search, out _))
+                results.RemoveAll(result => result.Title.Equals("Set Volume"));
         }
 
-        public void SetVolume(double value) {
-            value = Math.Max(0, Math.Min(100, value)); // ensure value is between 0 and 100
-            _volume.MasterVolumeLevelScalar = (float)value / 100;
-            currentVolume = _volume.MasterVolumeLevelScalar;
+        return results;
+    }
 
-            /*if (Regex.IsMatch(value, @"^\d")) {
-                results.Add(new Result {
-                    Title = "Invalid input",
-                    SubTitle = "Type 'vol' to see available commands",
-                    IcoPath = "Images/app.png"
-                });
-            }*/
-        }
+    private void AddMuteResult(List<Result> results)
+    {
+        results.Add(new Result
+        {
+            Title = "Mute",
+            SubTitle = "Mute the volume",
+            Action = _ =>
+            {
+                Mute();
+                return true;
+            }
+        });
+    }
 
-        public void Mute() {
-            if (isMute) return;
+    private void AddUnmuteResult(List<Result> results)
+    {
+        results.Add(new Result
+        {
+            Title = "Unmute",
+            SubTitle = "Restore the volume",
+            Action = _ =>
+            {
+                Unmute();
+                return true;
+            }
+        });
+    }
 
-            currentVolume = _volume.MasterVolumeLevelScalar;
-            _volume.MasterVolumeLevelScalar = 0;
+    private void AddSetVolumeResult(List<Result> results, double value)
+    {
+        var subTitle = value < 0 ? "Enter a custom value" : $"Set volume to {value}";
 
-            isMute = true;
-        }
+        results.Add(new Result
+        {
+            Title = "Set Volume",
+            SubTitle = subTitle,
+            Action = _ =>
+            {
+                SetVolume(value);
+                return true;
+            },
+            IcoPath = "Images/app.png"
+        });
+    }
 
-        public void Unmute() {
-            _volume.MasterVolumeLevelScalar = currentVolume;
-            isMute = false;
-        }
+    public void SetVolume(double value)
+    {
+        value = Math.Max(0, Math.Min(100, value));
+        _volume.MasterVolumeLevelScalar = (float)value / 100;
+        _currentVolume = _volume.MasterVolumeLevelScalar;
+    }
+
+    public void Mute()
+    {
+        if (_isMute) return;
+
+        _currentVolume = _volume.MasterVolumeLevelScalar;
+        _volume.MasterVolumeLevelScalar = 0;
+
+        _isMute = true;
+    }
+
+    public void Unmute()
+    {
+        _volume.MasterVolumeLevelScalar = _currentVolume;
+        _isMute = false;
     }
 }
